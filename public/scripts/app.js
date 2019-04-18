a73a0e23a46f1ca8ddbfd139f54b3cba992c9cbe
Mariana <mariana.horvat.qa@gmail.com>
2019-04-18 22:15:03 +0000

    Fetching tweets with Ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,61 +8,27 @@
 
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": {
-          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-        },
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": {
-          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-        },
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    },
-    {
-      "user": {
-        "name": "Johann von Goethe",
-        "avatars": {
-          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-        },
-        "handle": "@johann49"
-      },
-      "content": {
-        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-      },
-      "created_at": 1461113796368
-    }
-  ];
-  
+//----------------------------------------------------------------------------
+//RENDER TWEETS
+//----------------------------------------------------------------------------
+
   function renderTweets(tweets) {
+    var tweetContainer = $('.tweets-container')
+    tweetContainer.empty();
+
     for (tweet of tweets) {
       let $tweet = createTweetElement(tweet);
-      $('.tweets-container').append($tweet);
+      tweetContainer.prepend($tweet);
         // console.log(tweet);
     }
   }
   
+//-----------------------------------------------------------------------------
+//CREATE NEW TWEETS
+//-----------------------------------------------------------------------------
+// loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
     function createTweetElement(tweet) {
       let {user, content, created_at} = tweet;
       let {name, avatars, handle} = user;
@@ -106,36 +72,45 @@ $(document).ready(function() {
     return $tweet;
   }
   
-  renderTweets(data);
-  });
-      
-  // <article class="tweet">
-              
-  //             <header>
-  //               <img class="tweet-image" src="/images/bird.png">
-  //               <h3>Bill Fields</h3>
-  //               <p>@MrFields</p>
-  //             </header>
-
-  //             <section class="tweet-body">
-  //             <span>Little tweet here</span>
-  //             </section> 
-
-  //             <footer>
-  //               <span class="months">days ago</span>
-
-  //               <div class="icons">
-  //                 <i class="fa fa-flag" aria-hidden="true"></i>
-  //                 <i class="fa fa-retweet" aria-hidden="true"></i>
-  //                 <i class="fa fa-heart" aria-hidden="true"></i>
-  //               </div>
-    
-  //             </footer>
-            
-  //           </article>
-
   
+//-----------------------------------------------------------------------------
+//Create an AJAX POST request that sends the form data to the server
+//------------------------------------------------------------------------------
+$(function() {
+  //event.preventDefault() prevents form to submit through HTTP upon click
+  //need information from the tweet
+  //.seralize() to turn form data into query string
+  //send to server
+  let $newTweetForm = $(".new-tweet form");
+  console.log('First run')
 
-    // loops through tweets
-      // calls createTweetElement for each tweet
-      // takes return value and appends it to the tweets container
+  $newTweetForm.on('submit', function (event) {
+      event.preventDefault();
+      console.log( $( this ).serialize() );
+      //get the value of text-area and seralize form data into query string before sending to server
+      $.ajax({
+          type: 'POST',
+          url: '/tweets',
+          data: $( this ).serialize(),
+          success: function() {
+              loadTweets()
+              //renderTweets(data);
+          }
+      })
+   });
+  });
+
+   //----------------------------------------------------------------
+   //Fetch (GET) tweets from the server with Ajax
+   //----------------------------------------------------------------
+      function loadTweets() {
+        $.ajax( { 
+            method: 'GET',
+            url: '/tweets',
+            success: function(tweets){
+              (renderTweets(tweets))
+            }
+        });
+      }
+      loadTweets()
+});
